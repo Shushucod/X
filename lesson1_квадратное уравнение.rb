0e1f2e8ebd0bd6e625a8f9f111b3d82c0c03814d
Shushucod <64014588+Shushucod@users.noreply.github.com>
2020-04-21 20:58:18 +0300

    Update lesson1_квадратное уравнение.rb

--- lesson1_квадратное уравнение.rb
+++ lesson1_квадратное уравнение.rb
@@ -6,7 +6,7 @@ puts 'Введите коэффициент c'
 с = gets.chomp
 d = (b**2 – 4 * a * c)
 if d > 0
-   puts 'х1 = #{(–b + Math.sqrt(d))/(2 * a)} , х2 = #{(-b-Math.sqrt(d))/(2 * a)}'
+   puts 'х1 = #{(–b + Math.sqrt(d))/(2 * a)} , х2 = #{(-b - Math.sqrt(d))/(2 * a)}'
 elsif d == 0
    puts 'x1 = x2 = #{–b/(2 * a)}'
 else d < 0
